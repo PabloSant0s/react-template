bd2c47bb7ac02dcf25e2c3606435c720193db9e0
Pablo Santos <pablorasantos.dev@gmail.com>
2024-10-10 11:38:41 -0300

    feat: configura axios e react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
 import { ThemeProvider } from './components/theme/theme-provider'
+import { queryClient } from './lib/react-query'
 import { router } from './router'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
+      <Helmet titleTemplate="%s" />
       <ThemeProvider storageKey="react-theme" defaultTheme="dark">
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
